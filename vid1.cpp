7aaef0a94ded85ee497d042ffb86187f501ebd8c
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-07-28 08:36:10 +0530

    feat: Add code for creating and printing a pair in C++

--- vid1.cpp
+++ vid1.cpp
@@ -1,1 +1,13 @@
 // Pairs and Vectors: C++ STL Tutorial for Beginners | Competitive Programming Course
+// Date: 2021 09 07
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+
+    pair<int , string > p; // creating pair p with two 
+    //two ways to assign the values to pair
+    p = make_pair(2, "abc"); // assigning values to pair
+    p = {3, "hello"}; // assigning values to pair
+    cout<<p.first<<" "<<p.second<<endl; // printing pair values
+
+ }
